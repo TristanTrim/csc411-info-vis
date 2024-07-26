a1a0b88309727e1c2d61c162f477866689b75247
TristanTrim <TristanTrim@gmail.com>
2024-07-26 16:50:03 -0700

    Test commit worked : )

--- vis.js
+++ vis.js
@@ -213,6 +213,7 @@ window.drawMap = function(){
 };
 
 // simply adding a comment to confirm I know how to push & pull
+// ( it worked ! )
 
 // export stuff for access later & in web console.
 window.d3 = d3;
